fa8d17719dcd1d65e7b3e03b6dfcb5af50ced827
MgKyaw <mgkyaw20112@gmail.com>
2026-01-05 21:36:49 +0700

    feat: Add comment for JSX

--- Components/Creating and nesting components/JSX.js
+++ Components/Creating and nesting components/JSX.js
@@ -1,3 +1,8 @@
+// Writing markup with JSX 
+
+// JSX is stricter than HTML. You have to close tags like <br />. Your component also can’t return multiple JSX tags. You have to wrap them into a shared parent, like a <div>...</div> or an empty <>...</> wrapper:
+
+
 function AboutPage() {
   return (
     <>
