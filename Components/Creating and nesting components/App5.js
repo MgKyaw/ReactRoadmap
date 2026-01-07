ba7e0f64fdd9c4c3ca3dd62ceb6c585679a4c15b
MgKyaw <mgkyaw20112@gmail.com>
2026-01-07 21:57:47 +0700

    feat: Pass count and handleClick to MyButton

--- Components/Creating and nesting components/App5.js
+++ Components/Creating and nesting components/App5.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export default function MyApp() {
   const [count, setCount] = useState(0);
-  
+
   function handleClick() {
     setCount(count + 1);
   }
@@ -10,8 +10,8 @@ export default function MyApp() {
   return (
     <div>
       <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
