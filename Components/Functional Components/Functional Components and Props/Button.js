a6c72f992d750bcea54de1fdd0ba5d310566938d
MgKyaw <mgkyaw20112@gmail.com>
2026-01-09 21:42:18 +0700

    feat: Add Button2 for static defaultProps

--- Components/Functional Components/Functional Components and Props/Button.js
+++ Components/Functional Components/Functional Components and Props/Button.js
@@ -14,3 +14,13 @@ class Button extends Component {
 
 // static contextType 
 // If you want to read this.context from your class component, you must specify which context it needs to read. The context you specify as the static contextType must be a value previously created by createContext.
+
+class Button2 extends Component {
+  static defaultProps = {
+    color: 'blue'
+  };
+
+  render() {
+    return <button className={this.props.color}>click me</button>;
+  }
+}
