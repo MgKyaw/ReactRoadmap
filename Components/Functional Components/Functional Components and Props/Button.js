3b87a9b9c90358b920dbb2a2f5041e75b8f941d1
MgKyaw <mgkyaw20112@gmail.com>
2026-01-09 21:40:42 +0700

    feat: Add description for static contextType

--- Components/Functional Components/Functional Components and Props/Button.js
+++ Components/Functional Components/Functional Components and Props/Button.js
@@ -11,3 +11,6 @@ class Button extends Component {
     );
   }
 }
+
+// static contextType 
+// If you want to read this.context from your class component, you must specify which context it needs to read. The context you specify as the static contextType must be a value previously created by createContext.
