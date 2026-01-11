48b51eb4a18af9ae3b6cff341e40bad4a4a62de3
MgKyaw <mgkyaw20112@gmail.com>
2026-01-11 16:53:26 +0700

    feat: Add solution code for Challenge2

--- Components/Functional Components/Functional Components and Props/YourFirstComponent/Challenge2.js
+++ Components/Functional Components/Functional Components and Props/YourFirstComponent/Challenge2.js
@@ -1,4 +1,12 @@
+// export default function Profile() {
+//   return
+//     <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+// }
+
 export default function Profile() {
-  return
+  return (
+    <>
     <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+  </> 
+  );
 }
