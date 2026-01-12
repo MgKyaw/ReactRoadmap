2da7949f4b79b8749babd75c92f3f1591bd90896
MgKyaw <mgkyaw20112@gmail.com>
2026-01-12 23:00:23 +0700

    feat: Add Reading props inside the child component

--- Components/Functional Components/PassingPropsToAComponent/App.js
+++ Components/Functional Components/PassingPropsToAComponent/App.js
@@ -10,6 +10,19 @@ function Avatar() {
   );
 }
 
+// Step 2: Read props inside the child component 
+function Avatar({ person, size}) {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+}
+
 // export default function Profile() {
 //   return (
 //     <Avatar />
