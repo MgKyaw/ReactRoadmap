b07b597084c725fe1d479b75e16c4012c4ee923d
MgKyaw <mgkyaw20112@gmail.com>
2026-01-12 22:59:08 +0700

    feat: Add Step 1 Pass props to the child component

--- Components/Functional Components/PassingPropsToAComponent/App.js
+++ Components/Functional Components/PassingPropsToAComponent/App.js
@@ -10,8 +10,19 @@ function Avatar() {
   );
 }
 
+// export default function Profile() {
+//   return (
+//     <Avatar />
+//   );
+// }
+
+// Step 1: Pass props to the child component
+
 export default function Profile() {
   return (
-    <Avatar />
+    <Avatar
+      person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }}
+      size={100}
+    />
   );
 }
