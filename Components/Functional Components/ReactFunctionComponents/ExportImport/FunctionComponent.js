77a94fcd5eecfd40d8bb955c3280bf5cc193f4c4
MgKyaw <mgkyaw20112@gmail.com>
2026-01-19 09:29:12 +0700

    feat: Add Function component

---
+++ Components/Functional Components/ReactFunctionComponents/ExportImport/FunctionComponent.js
@@ -0,0 +1,23 @@
+// Function Component
+
+const App = () => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem('myValueInLocalStorage') || '',
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('myValueInLocalStorage', value);
+  }, [value]);
+
+  const onChange = event => setValue(event.target.value);
+
+  return (
+    <div>
+      <h1>Hello React Function Component!</h1>
+
+      <input value={value} type="text" onChange={onChange} />
+
+      <p>{value}</p>
+    </div>
+  );
+};
