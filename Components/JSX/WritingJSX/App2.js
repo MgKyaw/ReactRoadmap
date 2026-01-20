6494b8185ccdbba8700dd321d847d8f15fe66348
MgKyaw <mgkyaw20112@gmail.com>
2026-01-20 23:36:37 +0700

    feat: Add App2.js

--- Components/JSX/WritingJSX/App2.js
+++ Components/JSX/WritingJSX/App2.js
@@ -1,16 +1,11 @@
-export default function TodoList() {
-  return (
-    // This doesn't quite work!
-    <h1>Hedy Lamarr's Todos</h1>
-    <img 
-      src="https://i.imgur.com/yXOvdOSs.jpg" 
-      alt="Hedy Lamarr" 
-      class="photo"
-    >
-    <ul>
-      <li>Invent new traffic lights
-      <li>Rehearse a movie scene
-      <li>Improve the spectrum technology
-    </ul>
-  );
-}
+<div>
+  <h1>Hedy Lamarr's Todos</h1>
+  <img 
+    src="https://i.imgur.com/yXOvdOSs.jpg" 
+    alt="Hedy Lamarr" 
+    class="photo"
+  />
+  <ul>
+    
+  </ul>
+</div>
