eb73615cccaff23b25bcb5c650b2dc993b5cf9c8
MgKyaw <mgkyaw20112@gmail.com>
2026-01-20 23:38:53 +0700

    feat: Add tags

---
+++ Components/JSX/WritingJSX/CloseAllTags.js
@@ -0,0 +1,12 @@
+<>
+  <img 
+    src="https://i.imgur.com/yXOvdOSs.jpg" 
+    alt="Hedy Lamarr" 
+    class="photo"
+   />
+  <ul>
+    <li>Invent new traffic lights</li>
+    <li>Rehearse a movie scene</li>
+    <li>Improve the spectrum technology</li>
+  </ul>
+</>
