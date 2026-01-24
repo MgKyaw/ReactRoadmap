f5397f1bfc891b7390b907b7cad23d8ff0382be6
MgKyaw <mgkyaw20112@gmail.com>
2026-01-24 22:28:09 +0700

    feat: Comment challenge 4 code

--- Components/AComponentMemory/Challenge4.js
+++ Components/AComponentMemory/Challenge4.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function FeedbackForm() {
-  const [name, setName] = useState('');
+// export default function FeedbackForm() {
+//   const [name, setName] = useState('');
 
-  function handleClick() {
-    setName(prompt('What is your name?'));
-    alert(`Hello, ${name}!`);
-  }
+//   function handleClick() {
+//     setName(prompt('What is your name?'));
+//     alert(`Hello, ${name}!`);
+//   }
 
-  return (
-    <button onClick={handleClick}>
-      Greet
-    </button>
-  );
-}
+//   return (
+//     <button onClick={handleClick}>
+//       Greet
+//     </button>
+//   );
+// }
