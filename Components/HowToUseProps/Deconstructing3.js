89bcc3d6d6310ab5666d62733dd10acf3a1e5951
MgKyaw <mgkyaw20112@gmail.com>
2026-01-26 21:39:52 +0700

    feat: Add deconstructing 3

---
+++ Components/HowToUseProps/Deconstructing3.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <Welcome text="Welcome to React" />
+    </div>
+  );
+};
+
+const Welcome = ({ text }) => {
+  return <h1>{text}</h1>;
+};
