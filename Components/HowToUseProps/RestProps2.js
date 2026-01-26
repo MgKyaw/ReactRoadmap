62ce22abe175327a7124a125d6261579081de45c
MgKyaw <mgkyaw20112@gmail.com>
2026-01-26 21:46:30 +0700

    feat: Add rest props 2

---
+++ Components/HowToUseProps/RestProps2.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <Button
+        label={count}
+        disabled={true}
+        onClick={() => setCount(count + 1)}
+      />
+    </div>
+  );
+};
+
+const Button = ({ label, disabled, onClick }) => (
+  <button disabled={disabled} onClick={onClick}>
+    {label}
+  </button>
+);
+
+export default App;
