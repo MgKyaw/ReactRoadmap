30b5408ecaa20e8e2ef3adad5059156c959adb4b
MgKyaw <mgkyaw20112@gmail.com>
2026-01-27 21:00:02 +0700

    feat: Add welcome 2

--- Components/HowToUseProps/RestPropsWelcome.js
+++ Components/HowToUseProps/RestPropsWelcome.js
@@ -4,3 +4,10 @@ const Welcome = ({ title, description }) => (
     <Description description={description} />
   </div>
 );
+
+const Welcome2 = ({ title, description }) => (
+  <div>
+    <Title title={`Welcome to ${title || 'Earth'}`} />
+    <Description description={description} />
+  </div>
+);
