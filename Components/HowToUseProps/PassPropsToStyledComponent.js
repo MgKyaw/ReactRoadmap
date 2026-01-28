951bc307d8524f9e458161bedb7504a43d1d3930
MgKyaw <mgkyaw20112@gmail.com>
2026-01-28 22:34:47 +0700

    feat: Add new sample code for styled component

--- Components/HowToUseProps/PassPropsToStyledComponent.js
+++ Components/HowToUseProps/PassPropsToStyledComponent.js
@@ -1,3 +1,31 @@
+// import styled from 'styled-components';
+
+// const Input = styled.input`
+//   padding: 0.5em;
+//   margin: 0.5em;
+//   color: palevioletred;
+//   background: papayawhip;
+//   border: none;
+//   border-radius: 3px;
+// `;
+
+// const App = () => {
+//   const [value, setValue] = React.useState('');
+
+//   const onChange = (event) => {
+//     setValue(event.target.value);
+//   }
+
+//   return (
+//     <div>
+//       <Input
+//         value={value}
+//         onChange={onChange}
+//       />
+//     </div>
+//   );
+// }
+
 import styled from 'styled-components';
 
 const Input = styled.input`
@@ -6,7 +34,7 @@ const Input = styled.input`
   color: palevioletred;
   background: papayawhip;
   border: none;
-  border-radius: 3px;
+  border-radius: ${props => props.hasRadius ? '3px' : '0px'};
 `;
 
 const App = () => {
@@ -21,6 +49,7 @@ const App = () => {
       <Input
         value={value}
         onChange={onChange}
+        hasRadius={true}
       />
     </div>
   );
