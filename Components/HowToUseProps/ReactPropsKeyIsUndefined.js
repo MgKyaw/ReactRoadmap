2f26b6afa1d6aeaa64059055b02d4740b97cb2ac
MgKyaw <mgkyaw20112@gmail.com>
2026-01-28 22:31:29 +0700

    feat: Update React Props key is undefined

--- Components/HowToUseProps/ReactPropsKeyIsUndefined.js
+++ Components/HowToUseProps/ReactPropsKeyIsUndefined.js
@@ -1,9 +1,19 @@
+// const List = ({ users }) => (
+//   <ul>
+//     {users.map(user => <Item key={user.id}>{user.name}</Item>)}
+//   </ul>
+// );
+
+// const Item = ({ children }) => (
+//   <p>{children}</p>
+// );
+
 const List = ({ users }) => (
   <ul>
     {users.map(user => <Item key={user.id}>{user.name}</Item>)}
   </ul>
 );
 
-const Item = ({ children }) => (
-  <p>{children}</p>
+const Item = ({ key, children }) => (
+  <p>{key} {children}</p>
 );
