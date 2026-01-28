518ac62b9af42ca0c9a930e1a15015bafb006886
MgKyaw <mgkyaw20112@gmail.com>
2026-01-28 22:32:09 +0700

    feat: Add third sample code for react props key

--- Components/HowToUseProps/ReactPropsKeyIsUndefined.js
+++ Components/HowToUseProps/ReactPropsKeyIsUndefined.js
@@ -8,12 +8,26 @@
 //   <p>{children}</p>
 // );
 
+// const List = ({ users }) => (
+//   <ul>
+//     {users.map(user => <Item key={user.id}>{user.name}</Item>)}
+//   </ul>
+// );
+
+// const Item = ({ key, children }) => (
+//   <p>{key} {children}</p>
+// );
+
 const List = ({ users }) => (
   <ul>
-    {users.map(user => <Item key={user.id}>{user.name}</Item>)}
+    {users.map(user => (
+      <Item key={user.id} id={user.id}>
+        {user.name}
+      </Item>
+    ))}
   </ul>
 );
 
-const Item = ({ key, children }) => (
-  <p>{key} {children}</p>
+const Item = ({ id, children }) => (
+  <p>{id} {children}</p>
 );
