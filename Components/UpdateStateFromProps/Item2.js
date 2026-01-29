5e9ac6fc9fa6888ec05f2e4ef35c9b501b26c12d
MgKyaw <mgkyaw20112@gmail.com>
2026-01-29 21:29:35 +0700

    feat: Use UseEffect at Item function

--- Components/UpdateStateFromProps/Item2.js
+++ Components/UpdateStateFromProps/Item2.js
@@ -1,3 +1,22 @@
+// function Item({ item, onUpdateName }) {
+//   // derive initial state from props
+//   const [name, setName] = React.useState(item.name + '!');
+
+//   function handleNameChange(event) {
+//     setName(event.target.value);
+//   }
+
+//   return (
+//     <li>
+//       {item.name}
+//       <input type="text" value={name} onChange={handleNameChange} />
+//       <button type="button" onClick={() => onUpdateName(item, name)}>
+//         Update
+//       </button>
+//     </li>
+//   );
+// }
+
 function Item({ item, onUpdateName }) {
   // derive initial state from props
   const [name, setName] = React.useState(item.name + '!');
@@ -5,6 +24,11 @@ function Item({ item, onUpdateName }) {
   function handleNameChange(event) {
     setName(event.target.value);
   }
+
+  // derive updated state from props
+  React.useEffect(() => {
+    setName(item.name + '!');
+  }, [item]);
 
   return (
     <li>
