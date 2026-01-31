a54cff82294701bfae02776feaad3fb261ef18ee
MgKyaw <mgkyaw20112@gmail.com>
2026-01-31 22:09:11 +0700

    feat: Add solution code

--- ConditionalRendering/ChallengeCode2.js
+++ ConditionalRendering/ChallengeCode2.js
@@ -1,7 +1,11 @@
 function Item({ name, importance }) {
   return (
     <li className="item">
-      {name} {importance > 0 && "(Importance: " + importance + ")"}
+      {name}
+      {importance > 0 && ' '}
+      {importance > 0 &&
+        <i>(Importance: {importance})</i>
+      }
     </li>
   );
 }
