d131cd7be3c105a9c294c439fd55aca1a7d9e608
MgKyaw <mgkyaw20112@gmail.com>
2026-01-31 22:34:59 +0700

    feat: Add React IF ELSE

---
+++ DifferentTechniques/React_IF_ELSE.js
@@ -0,0 +1,17 @@
+function List({ list }) {
+  if (!list) {
+    return null;
+  }
+
+  if (!list.length) {
+    return <p>Sorry, the list is empty.</p>;
+  } else {
+    return (
+      <div>
+        {list.map(item => (
+          <Item item={item} />
+        ))}
+      </div>
+    );
+  }
+}
