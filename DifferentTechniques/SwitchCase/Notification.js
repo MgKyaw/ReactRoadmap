ad195c50cf402ca0175e849f28d366e270c91fdb
MgKyaw <mgkyaw20112@gmail.com>
2026-02-01 23:48:58 +0700

    fix: Update Notication

--- DifferentTechniques/SwitchCase/Notification.js
+++ DifferentTechniques/SwitchCase/Notification.js
@@ -1,3 +1,15 @@
-function LoadingIndicator({ isLoading }) {
-  return <div>{isLoading && <p>Loading...</p>}</div>;
+function Notification({ text, status }) {
+  if (status === 'info') {
+    return <Info text={text} />;
+  }
+
+  if (status === 'warning') {
+    return <Warning text={text} />;
+  }
+
+  if (status === 'error') {
+    return <Error text={text} />;
+  }
+
+  return null;
 }
