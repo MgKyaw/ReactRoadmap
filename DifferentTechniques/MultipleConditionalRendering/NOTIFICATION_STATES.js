d3b31e74788f8fb214156de1068d00ed0550b887
MgKyaw <mgkyaw20112@gmail.com>
2026-02-02 23:44:53 +0700

    feat: Add Notification with text and status props

--- DifferentTechniques/MultipleConditionalRendering/NOTIFICATION_STATES.js
+++ DifferentTechniques/MultipleConditionalRendering/NOTIFICATION_STATES.js
@@ -3,3 +3,17 @@ const NOTIFICATION_STATES = {
   warning: 'Be careful here ...',
   error: 'Something went wrong ...',
 };
+
+function Notification({ text, status }) {
+  return (
+    <div>
+      {
+        {
+          info: <Info text={text} />,
+          warning: <Warning text={text} />,
+          error: <Error text={text} />,
+        }[status]
+      }
+    </div>
+  );
+}
