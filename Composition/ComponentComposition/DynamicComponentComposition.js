0c0556bdfa738e9c6af09e674dc909624114fa80
MgKyaw <mgkyaw20112@gmail.com>
2026-02-08 18:08:28 +0700

    feat: Add dynamic component composition

---
+++ Composition/ComponentComposition/DynamicComponentComposition.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import Navigation from './Navigation';
+import LandingPage from './Landing';
+import SignUpPage from './SignUp';
+import SignInPage from './SignIn';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path='/' component={LandingPage} />
+      <Route path='/register' component={SignUpPage} />
+      <Route path='/login' component={SignInPage} />
+
+      <Footer />
+    </div>
+  </Router>
+);
