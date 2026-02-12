92857ca6a47155a4168612f4ae2506c65e7b3a1e
MgKyaw <mgkyaw20112@gmail.com>
2026-02-12 22:54:04 +0700

    feat: Add App with Greeting

---
+++ Rendering/RenderingInReact/GreetingApp.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+function Greeting ({ name }) {
+  const [index, setIndex] = React.useState(0)
+
+  const greetings = ['Hello', "Hola", "Bonjour"]
+
+  const handleClick = () => {
+    const nextIndex = index === greetings.length - 1
+      ? 0
+      : index + 1
+    setIndex(nextIndex)
+  }
+
+  return (
+    <main>
+      <h1>{greetings[index]}, {name}</h1>
+      <button onClick={handleClick}>Next Greeting</button>
+    </main>
+  )
+}
+
+export default function App () {
+  return <Greeting name="Tyler" />
+}
