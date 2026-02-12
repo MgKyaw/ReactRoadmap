c6312499cb8af25c3bc24efb255023cb5272ac8c
MgKyaw <mgkyaw20112@gmail.com>
2026-02-12 22:55:38 +0700

    feat: Modify Counter.js

--- Rendering/RenderingInReact/Counter.js
+++ Rendering/RenderingInReact/Counter.js
@@ -1,17 +1,18 @@
-import * as React from "react"
-
 export default function Counter () {
-  console.count("Rendering Counter")
   const [count, setCount] = React.useState(0)
 
   const handleClick = () => {
-    console.count("click")
-    setCount(count)
+    setCount(count + 1)
+    setCount(count + 1)
+    setCount(count + 1)
   }
 
   return (
-    <button onClick={handleClick}>
-      🤨
-    </button>
+    <main>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>
+        +
+      </button>
+    </main>
   )
 }
