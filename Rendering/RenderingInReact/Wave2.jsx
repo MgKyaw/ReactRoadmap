58c35119fbf48142afca83785a144677144bb9a6
MgKyaw <mgkyaw20112@gmail.com>
2026-02-13 19:55:19 +0700

    feat: Add Wave2.jsx

---
+++ Rendering/RenderingInReact/Wave2.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+function Wave () {
+  console.count('Rendering Wave')
+  return (
+    <span role="img" aria-label="hand waving">
+      👋
+    </span>
+  )
+}
+
+export default React.memo(Wave)
