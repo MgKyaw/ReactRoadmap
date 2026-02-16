2edaf5e1f6c9fd81558a3384cfa56bee9943f20c
MgKyaw <mgkyaw20112@gmail.com>
2026-02-16 21:06:13 +0700

    feat: Add solution code for chat.js

--- Rendering/Component Lifecycle/Lifecycle Of React Effects/Challenge Code 4/chat.js
+++ Rendering/Component Lifecycle/Lifecycle Of React Effects/Challenge Code 4/chat.js
@@ -46,3 +46,28 @@ export function createUnencryptedConnection(roomId) {
 //     }
 //   };
 // }
+
+////chat.js
+// export function createEncryptedConnection(roomId) {
+//   // A real implementation would actually connect to the server
+//   return {
+//     connect() {
+//       console.log('✅ 🔐 Connecting to "' + roomId + '... (encrypted)');
+//     },
+//     disconnect() {
+//       console.log('❌ 🔐 Disconnected from "' + roomId + '" room (encrypted)');
+//     }
+//   };
+// }
+
+// export function createUnencryptedConnection(roomId) {
+//   // A real implementation would actually connect to the server
+//   return {
+//     connect() {
+//       console.log('✅ Connecting to "' + roomId + '... (unencrypted)');
+//     },
+//     disconnect() {
+//       console.log('❌ Disconnected from "' + roomId + '" room (unencrypted)');
+//     }
+//   };
+// }
