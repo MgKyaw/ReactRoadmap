ece4fc20ca761f1b647e699a38af84d51f1954d9
MgKyaw <mgkyaw20112@gmail.com>
2026-02-16 21:03:30 +0700

    feat: Add solution code

--- Rendering/Component Lifecycle/Lifecycle Of React Effects/Challenge Code 4/ChatRoom.js
+++ Rendering/Component Lifecycle/Lifecycle Of React Effects/Challenge Code 4/ChatRoom.js
@@ -10,3 +10,16 @@ export default function ChatRoom({ roomId, createConnection }) {
 
   return <h1>Welcome to the {roomId} room!</h1>;
 }
+
+//// Solution Code
+// import { useState, useEffect } from 'react';
+
+// export default function ChatRoom({ roomId, createConnection }) {
+//   useEffect(() => {
+//     const connection = createConnection(roomId);
+//     connection.connect();
+//     return () => connection.disconnect();
+//   }, [roomId, createConnection]);
+
+//   return <h1>Welcome to the {roomId} room!</h1>;
+// }
