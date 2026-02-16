198403d7453f7b8047ff0b44ac4618a9ee2d72a3
MgKyaw <mgkyaw20112@gmail.com>
2026-02-16 21:05:38 +0700

    feat: Add chatroom.js

--- Rendering/Component Lifecycle/Lifecycle Of React Effects/Challenge Code 4/ChatRoom.js
+++ Rendering/Component Lifecycle/Lifecycle Of React Effects/Challenge Code 4/ChatRoom.js
@@ -23,3 +23,23 @@ export default function ChatRoom({ roomId, createConnection }) {
 
 //   return <h1>Welcome to the {roomId} room!</h1>;
 // }
+
+//// Solution Code 2
+// import { useState, useEffect } from 'react';
+// import {
+//   createEncryptedConnection,
+//   createUnencryptedConnection,
+// } from './chat.js';
+
+// export default function ChatRoom({ roomId, isEncrypted }) {
+//   useEffect(() => {
+//     const createConnection = isEncrypted ?
+//       createEncryptedConnection :
+//       createUnencryptedConnection;
+//     const connection = createConnection(roomId);
+//     connection.connect();
+//     return () => connection.disconnect();
+//   }, [roomId, isEncrypted]);
+
+//   return <h1>Welcome to the {roomId} room!</h1>;
+// }
