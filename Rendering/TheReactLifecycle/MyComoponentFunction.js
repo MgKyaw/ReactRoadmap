8160f96a9216105ba455b14002c2af7656086876
MgKyaw <mgkyaw20112@gmail.com>
2026-02-22 12:06:12 +0700

    feat: Add UseEffect

--- Rendering/TheReactLifecycle/MyComoponentFunction.js
+++ Rendering/TheReactLifecycle/MyComoponentFunction.js
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
+
 function MyComponent(props) {
 	const [count, setCount] = useState(0);
 	const [counterWeight, setCounterWeight] = useState(1);
 	const [themeMode, setThemeMode] = useState('light');
 
 	// the rest of the code...
+    useEffect(() => {
+		const { subscribe } = props.store;
+		const unsubscribe = subscribe(/*...*/);
+		return unsubscribe
+	}, []);
 
 	return (
 		<div>
