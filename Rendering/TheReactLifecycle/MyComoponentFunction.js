3e9b01a9a23a8a5b0829eb38650adfd12d7ec07f
MgKyaw <mgkyaw20112@gmail.com>
2026-02-22 12:04:48 +0700

    feat: Add MyComponent function

---
+++ Rendering/TheReactLifecycle/MyComoponentFunction.js
@@ -0,0 +1,14 @@
+function MyComponent(props) {
+	const [count, setCount] = useState(0);
+	const [counterWeight, setCounterWeight] = useState(1);
+	const [themeMode, setThemeMode] = useState('light');
+
+	// the rest of the code...
+
+	return (
+		<div>
+			...
+		</div>
+	)
+}
+
