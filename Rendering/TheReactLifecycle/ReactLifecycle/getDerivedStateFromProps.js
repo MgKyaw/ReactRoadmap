a40d7e9bef367b1f9e25b896b7a9658a37ed424d
MgKyaw <mgkyaw20112@gmail.com>
2026-02-23 19:46:59 +0700

    feat: Comment getDerivedStateFromProps

--- Rendering/TheReactLifecycle/ReactLifecycle/getDerivedStateFromProps.js
+++ Rendering/TheReactLifecycle/ReactLifecycle/getDerivedStateFromProps.js
@@ -1,23 +1,23 @@
-class MyComponent extends React.Component {
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.value !== prevState.value) {
-      return {
-        value: nextProps.value
-      };
-    }
-    return null;
-  }
+// class MyComponent extends React.Component {
+//   static getDerivedStateFromProps(nextProps, prevState) {
+//     if (nextProps.value !== prevState.value) {
+//       return {
+//         value: nextProps.value
+//       };
+//     }
+//     return null;
+//   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: props.value
+//     };
+//   }
 
-  render() {
-    return < div > {
-      this.state.value
-    } < /div>;
-  }
-}
+//   render() {
+//     return < div > {
+//       this.state.value
+//     } < /div>;
+//   }
+// }
