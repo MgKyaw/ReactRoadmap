5d1c3a0a94456de274ebcf900ee8b7f47f077296
MgKyaw <mgkyaw20112@gmail.com>
2026-02-25 22:11:04 +0700

    feat: Add WeatherApp.js

---
+++ Rendering/TheReactLifecycle/ReactLifecycle/weather-app/src/WeatherApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WeatherApp extends Component {
+ render() {
+  return (
+   <div>
+    <h1>Current Weather</h1>
+   </div>
+  );
+ }
+}
+
+export default WeatherApp;
