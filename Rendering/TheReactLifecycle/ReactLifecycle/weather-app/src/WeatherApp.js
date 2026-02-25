02bad978ba9554eadf14c2ce8984321e525d8b5b
MgKyaw <mgkyaw20112@gmail.com>
2026-02-25 22:11:51 +0700

    feat: Update WeatherApp with componentDidMount

--- Rendering/TheReactLifecycle/ReactLifecycle/weather-app/src/WeatherApp.js
+++ Rendering/TheReactLifecycle/ReactLifecycle/weather-app/src/WeatherApp.js
@@ -1,10 +1,48 @@
+// import React, { Component } from 'react';
+
+// class WeatherApp extends Component {
+//  render() {
+//   return (
+//    <div>
+//     <h1>Current Weather</h1>
+//    </div>
+//   );
+//  }
+// }
+
+// export default WeatherApp;
+
 import React, { Component } from 'react';
 
 class WeatherApp extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   latitude: null,
+   longitude: null
+  };
+ }
+
+ componentDidMount() {
+  navigator.geolocation.getCurrentPosition(
+   (position) => {
+    const { latitude, longitude } = position.coords;
+    this.setState({ latitude, longitude });
+   },
+   (error) => {
+    console.error(error);
+   }
+  );
+ }
+
  render() {
+  const { latitude, longitude } = this.state;
+
   return (
    <div>
     <h1>Current Weather</h1>
+    <p>Latitude: {latitude}</p>
+    <p>Longitude: {longitude}</p>
    </div>
   );
  }
