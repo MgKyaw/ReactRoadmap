fbae366e6ace26e02605bd4b5ba01c4567360876
MgKyaw <mgkyaw20112@gmail.com>
2026-02-28 20:13:17 +0700

    feat: Pass props to the child component

--- Rendering/RenderProps/RenderProps/Profile.js
+++ Rendering/RenderProps/RenderProps/Profile.js
@@ -10,8 +10,17 @@ function Avatar() {
   );
 }
 
+// export default function Profile() {
+//   return (
+//     <Avatar />
+//   );
+// }
+
 export default function Profile() {
   return (
-    <Avatar />
+    <Avatar
+      person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }}
+      size={100}
+    />
   );
 }
