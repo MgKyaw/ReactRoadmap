6d37d39a43bbbba1479a1e80442869cdbf6427b1
MgKyaw <mgkyaw20112@gmail.com>
2026-03-03 20:26:26 +0700

    feat: Add utils.js for Solution code 2

--- Rendering/RenderProps/RenderProps/ChallengeCode1/App.js
+++ Rendering/RenderProps/RenderProps/ChallengeCode1/App.js
@@ -232,3 +232,13 @@ export default function Gallery() {
 //     </div>
 //   );
 // }
+
+// //utils.js
+// export function getImageUrl(imageId, size = 's') {
+//   return (
+//     'https://i.imgur.com/' +
+//     imageId +
+//     size +
+//     '.jpg'
+//   );
+// }
