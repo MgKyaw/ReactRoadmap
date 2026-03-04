71badd70c3845f4fe5e67af73f069bf1d4b6ef13
MgKyaw <mgkyaw20112@gmail.com>
2026-03-04 20:46:58 +0700

    feat: Add Euro and Pound

--- Rendering/RenderProps/ReactRenderProps/Amount.js
+++ Rendering/RenderProps/ReactRenderProps/Amount.js
@@ -1,5 +1,9 @@
 const App = () => <Amount />;
 
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
+
 class Amount extends Component {
   constructor(props) {
     super(props);
