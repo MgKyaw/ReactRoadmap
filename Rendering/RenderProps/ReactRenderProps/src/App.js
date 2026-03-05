656c7b0aad33a825711f9321076bae75014e745c
MgKyaw <mgkyaw20112@gmail.com>
2026-03-05 21:09:51 +0700

    feat: Add App.js

--- Rendering/RenderProps/ReactRenderProps/src/App.js
+++ Rendering/RenderProps/ReactRenderProps/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+// const App = () => (
+//   <div>
+//     <h1>Currency Converter</h1>
+//   </div>
+// );
+
 const App = () => (
-  <div>
-    <h1>Currency Converter</h1>
-  </div>
+  <Amount>
+    <Pound amount={amount} />
+    <Euro amount={amount} />
+  </Amount>
 );
 
 export default App;
