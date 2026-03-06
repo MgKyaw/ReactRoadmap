8011cb9dfe4fc74336b08f223903e414a01d86f5
MgKyaw <mgkyaw20112@gmail.com>
2026-03-06 22:10:08 +0700

    feat: Add index3.js

---
+++ Rendering/RenderProps/Render Props Pattern/index3.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles.css";
+
+const Title = (props) => (
+  <>
+    {props.renderFirstComponent()}
+    {props.renderSecondComponent()}
+    {props.renderThirdComponent()}
+  </>
+);
+
+render(
+  <div className="App">
+    <Title
+      renderFirstComponent={() => <h1>✨ First render prop! ✨</h1>}
+      renderSecondComponent={() => <h2>🔥 Second render prop! 🔥</h2>}
+      renderThirdComponent={() => <h3>🚀 Third render prop! 🚀</h3>}
+    />
+  </div>,
+  document.getElementById("root")
+);
