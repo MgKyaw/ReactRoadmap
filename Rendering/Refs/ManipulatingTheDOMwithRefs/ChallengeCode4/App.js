1c8a01ad2337a51ffc940f7cae615d957d043f7f
MgKyaw <mgkyaw20112@gmail.com>
2026-03-14 20:31:53 +0700

    feat: Comment the wrong solution code

--- Rendering/Refs/ManipulatingTheDOMwithRefs/ChallengeCode4/App.js
+++ Rendering/Refs/ManipulatingTheDOMwithRefs/ChallengeCode4/App.js
@@ -12,57 +12,57 @@
 //   );
 // }
 
-// My Solution code
+// // My Solution code
 
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
+// import { people } from './data.js';
+// import { getImageUrl } from './utils.js';
 
-export default function List() {
-  const chemists = people.filter(person =>
-    person.profession === 'chemist'
-  );
-  const chemistsListItems = chemists.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
-  const others = people.filter(person =>
-    person.profession !== 'chemist'
-  );
-  const otherListItems = others.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
-  return (
-    <>
-      <article>
-        <h1>Chemists</h1>
-        <ul>{chemistsListItems}</ul>
-      </article>
-      <article>
-        <h1>Everyone Else</h1>
-        <ul>{otherListItems}</ul>
-      </article>
-    </>
-  );
-}
+// export default function List() {
+//   const chemists = people.filter(person =>
+//     person.profession === 'chemist'
+//   );
+//   const chemistsListItems = chemists.map(person =>
+//     <li key={person.id}>
+//       <img
+//         src={getImageUrl(person)}
+//         alt={person.name}
+//       />
+//       <p>
+//         <b>{person.name}:</b>
+//         {' ' + person.profession + ' '}
+//         known for {person.accomplishment}
+//       </p>
+//     </li>
+//   );
+//   const others = people.filter(person =>
+//     person.profession !== 'chemist'
+//   );
+//   const otherListItems = others.map(person =>
+//     <li key={person.id}>
+//       <img
+//         src={getImageUrl(person)}
+//         alt={person.name}
+//       />
+//       <p>
+//         <b>{person.name}:</b>
+//         {' ' + person.profession + ' '}
+//         known for {person.accomplishment}
+//       </p>
+//     </li>
+//   );
+//   return (
+//     <>
+//       <article>
+//         <h1>Chemists</h1>
+//         <ul>{chemistsListItems}</ul>
+//       </article>
+//       <article>
+//         <h1>Everyone Else</h1>
+//         <ul>{otherListItems}</ul>
+//       </article>
+//     </>
+//   );
+// }
 
 // Solution code
 // import { people } from './data.js';
