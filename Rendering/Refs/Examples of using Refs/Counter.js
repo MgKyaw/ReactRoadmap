ee39293ab4d4d27dd841419d94d33418a6dfb160
MgKyaw <mgkyaw20112@gmail.com>
2026-03-15 10:14:38 +0700

    feat: Comment out some

--- Rendering/Refs/Examples of using Refs/Counter.js
+++ Rendering/Refs/Examples of using Refs/Counter.js
@@ -18,19 +18,49 @@
 //   );
 // }
 
+// function Counter() {
+//   const hasClickedButton = React.useRef(false);
+
+//   const [count, setCount] = React.useState(0);
+
+//   function onClick() {
+//     const newCount = count + 1;
+
+//     setCount(newCount);
+
+//     hasClickedButton.current = true;
+//   }
+
+//   console.log('Has clicked button? ' + hasClickedButton.current);
+
+//   return (
+//     <div>
+//       <p>{count}</p>
+
+//       <button type="button" onClick={onClick}>
+//         Increase
+//       </button>
+//     </div>
+//   );
+// }
+
+// Comment out some
 function Counter() {
   const hasClickedButton = React.useRef(false);
 
   const [count, setCount] = React.useState(0);
 
   function onClick() {
-    const newCount = count + 1;
+    // const newCount = count + 1;
 
-    setCount(newCount);
+    // setCount(newCount);
 
     hasClickedButton.current = true;
   }
 
+  // Does only run for the first render.
+  // Component does not render again, because no state is set anymore.
+  // Only the ref's current property is set, which does not trigger a re-render.
   console.log('Has clicked button? ' + hasClickedButton.current);
 
   return (
