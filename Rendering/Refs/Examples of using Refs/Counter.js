35dde06d5f6a3410b35e6bea57fc076a97b4c51f
MgKyaw <mgkyaw20112@gmail.com>
2026-03-15 10:13:42 +0700

    feat: Use ref

--- Rendering/Refs/Examples of using Refs/Counter.js
+++ Rendering/Refs/Examples of using Refs/Counter.js
@@ -1,11 +1,37 @@
+// function Counter() {
+//   const [count, setCount] = React.useState(0);
+
+//   function onClick() {
+//     const newCount = count + 1;
+
+//     setCount(newCount);
+//   }
+
+//   return (
+//     <div>
+//       <p>{count}</p>
+
+//       <button type="button" onClick={onClick}>
+//         Increase
+//       </button>
+//     </div>
+//   );
+// }
+
 function Counter() {
+  const hasClickedButton = React.useRef(false);
+
   const [count, setCount] = React.useState(0);
 
   function onClick() {
     const newCount = count + 1;
 
     setCount(newCount);
+
+    hasClickedButton.current = true;
   }
+
+  console.log('Has clicked button? ' + hasClickedButton.current);
 
   return (
     <div>
