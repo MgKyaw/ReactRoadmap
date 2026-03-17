d55a9b9bb09944b984d890b167c1da0f224339d7
MgKyaw <mgkyaw20112@gmail.com>
2026-03-17 22:42:56 +0700

    feat: Add ReactCallback

--- Rendering/Refs/Examples of using Refs/ReactCallback.js
+++ Rendering/Refs/Examples of using Refs/ReactCallback.js
@@ -1,3 +1,28 @@
+// function ComponentWithRefRead() {
+//   const [text, setText] = React.useState('Some text ...');
+
+//   function handleOnChange(event) {
+//     setText(event.target.value);
+//   }
+
+//   const ref = React.useCallback((node) => {
+//     if (!node) return;
+
+//     const { width } = node.getBoundingClientRect();
+
+//     document.title = `Width:${width}`;
+//   }, []);
+
+//   return (
+//     <div>
+//       <input type="text" value={text} onChange={handleOnChange} />
+//       <div>
+//         <span ref={ref}>{text}</span>
+//       </div>
+//     </div>
+//   );
+// }
+
 function ComponentWithRefRead() {
   const [text, setText] = React.useState('Some text ...');
 
@@ -11,7 +36,7 @@ function ComponentWithRefRead() {
     const { width } = node.getBoundingClientRect();
 
     document.title = `Width:${width}`;
-  }, []);
+  }, [text]);
 
   return (
     <div>
