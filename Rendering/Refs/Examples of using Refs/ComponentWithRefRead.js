fc5dea57c5e6345c566f17659a35659cf7fada97
MgKyaw <mgkyaw20112@gmail.com>
2026-03-17 22:40:39 +0700

    feat: Add ComponentWithRefRead

---
+++ Rendering/Refs/Examples of using Refs/ComponentWithRefRead.js
@@ -0,0 +1,24 @@
+function ComponentWithRefRead() {
+  const [text, setText] = React.useState('Some text ...');
+
+  function handleOnChange(event) {
+    setText(event.target.value);
+  }
+
+  const ref = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = ref.current.getBoundingClientRect();
+
+    document.title = `Width:${width}`;
+  }, []);
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={handleOnChange} />
+      <div>
+        <span ref={ref}>{text}</span>
+      </div>
+    </div>
+  );
+}
