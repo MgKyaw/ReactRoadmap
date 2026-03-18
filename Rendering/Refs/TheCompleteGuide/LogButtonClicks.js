30fd410851bd83b28ea862f4fcd723b75017f5ce
MgKyaw <mgkyaw20112@gmail.com>
2026-03-18 21:20:19 +0700

    feat: Use useState

--- Rendering/Refs/TheCompleteGuide/LogButtonClicks.js
+++ Rendering/Refs/TheCompleteGuide/LogButtonClicks.js
@@ -1,11 +1,27 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
+
+// function LogButtonClicks() {
+//   const countRef = useRef(0);
+  
+//   const handle = () => {
+//     countRef.current++;
+//     console.log(`Clicked ${countRef.current} times`);
+//   };
+
+//   console.log('I rendered!');
+
+//   return <button onClick={handle}>Click me</button>;
+// }
+
+import { useState } from 'react';
 
 function LogButtonClicks() {
-  const countRef = useRef(0);
+  const [count, setCount] = useState(0);
   
   const handle = () => {
-    countRef.current++;
-    console.log(`Clicked ${countRef.current} times`);
+    const updatedCount = count + 1;
+    console.log(`Clicked ${updatedCount} times`);
+    setCount(updatedCount);
   };
 
   console.log('I rendered!');
