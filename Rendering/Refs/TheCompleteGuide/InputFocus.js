40dc911ae201d4754eeff2bd1bd6e8ed4570d2f6
MgKyaw <mgkyaw20112@gmail.com>
2026-03-19 21:41:20 +0700

    feat: Modify InputFocus

--- Rendering/Refs/TheCompleteGuide/InputFocus.js
+++ Rendering/Refs/TheCompleteGuide/InputFocus.js
@@ -1,16 +1,34 @@
+// import { useRef, useEffect } from 'react';
+
+// function InputFocus() {
+//   const inputRef = useRef();
+
+//   useEffect(() => {
+//     inputRef.current.focus();
+//   }, []);
+
+//   return (
+//     <input 
+//       ref={inputRef} 
+//       type="text" 
+//     />
+//   );
+// }
+
 import { useRef, useEffect } from 'react';
 
 function InputFocus() {
   const inputRef = useRef();
 
   useEffect(() => {
+    // Logs `HTMLInputElement` 
+    console.log(inputRef.current);
+
     inputRef.current.focus();
   }, []);
 
-  return (
-    <input 
-      ref={inputRef} 
-      type="text" 
-    />
-  );
+  // Logs `undefined` during initial rendering
+  console.log(inputRef.current);
+
+  return <input ref={inputRef} type="text" />;
 }
