25b937bf268546c593010fae8298acbf17ec24bc
MgKyaw <mgkyaw20112@gmail.com>
2026-03-23 20:58:05 +0700

    feat: Add CSS Animation

---
+++ Rendering/Events/ReactEventObject/CSSAnimation.js
@@ -0,0 +1,5 @@
+<div
+  onAnimationStart={e => console.log('onAnimationStart')}
+  onAnimationIteration={e => console.log('onAnimationIteration')}
+  onAnimationEnd={e => console.log('onAnimationEnd')}
+/>
