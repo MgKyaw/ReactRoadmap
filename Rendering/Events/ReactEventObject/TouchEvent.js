21d5a975b1f4de00a898e1dae5bad151a2c9946a
MgKyaw <mgkyaw20112@gmail.com>
2026-03-24 23:04:53 +0700

    feat: Add TouchEvent

---
+++ Rendering/Events/ReactEventObject/TouchEvent.js
@@ -0,0 +1,6 @@
+<div
+  onTouchStart={e => console.log('onTouchStart')}
+  onTouchMove={e => console.log('onTouchMove')}
+  onTouchEnd={e => console.log('onTouchEnd')}
+  onTouchCancel={e => console.log('onTouchCancel')}
+/>
