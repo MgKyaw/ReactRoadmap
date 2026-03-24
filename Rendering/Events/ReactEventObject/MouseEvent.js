e6addbc7cff315c3a37ff7e5cd11602f662cf0dd
MgKyaw <mgkyaw20112@gmail.com>
2026-03-24 23:03:52 +0700

    feat: Add MouseEvent

---
+++ Rendering/Events/ReactEventObject/MouseEvent.js
@@ -0,0 +1,8 @@
+<div
+  onClick={e => console.log('onClick')}
+  onMouseEnter={e => console.log('onMouseEnter')}
+  onMouseOver={e => console.log('onMouseOver')}
+  onMouseDown={e => console.log('onMouseDown')}
+  onMouseUp={e => console.log('onMouseUp')}
+  onMouseLeave={e => console.log('onMouseLeave')}
+/>
