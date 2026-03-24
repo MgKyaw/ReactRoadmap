1b79b20b81f835dc766a924843d5e4665ab177a3
MgKyaw <mgkyaw20112@gmail.com>
2026-03-24 23:04:28 +0700

    feat: Add Pointer event

---
+++ Rendering/Events/ReactEventObject/PointerEvent.js
@@ -0,0 +1,7 @@
+<div
+  onPointerEnter={e => console.log('onPointerEnter')}
+  onPointerMove={e => console.log('onPointerMove')}
+  onPointerDown={e => console.log('onPointerDown')}
+  onPointerUp={e => console.log('onPointerUp')}
+  onPointerLeave={e => console.log('onPointerLeave')}
+/>
