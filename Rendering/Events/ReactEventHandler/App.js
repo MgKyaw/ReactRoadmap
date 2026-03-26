f85a78edc950de066458af51b44dbeafdee9c489
MgKyaw <mgkyaw20112@gmail.com>
2026-03-26 23:12:17 +0700

    feat: Add App.js

---
+++ Rendering/Events/ReactEventHandler/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  function handleClick() {
+    console.log('Button click ...');
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        Event Handler
+      </button>
+    </div>
+  );
+}
