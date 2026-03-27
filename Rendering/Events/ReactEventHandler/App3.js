d150500401890242293b4811e4aee40eb4a6e3dc
MgKyaw <mgkyaw20112@gmail.com>
2026-03-27 23:20:32 +0700

    feat: Add App3

---
+++ Rendering/Events/ReactEventHandler/App3.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  const user = {
+    id: '123abc',
+    username: 'Robin Wieruch',
+  };
+
+  function handleUserSignIn() {
+    // do something
+  }
+
+  function handleUserSignUp() {
+    // do something
+  }
+
+  function handleUserSignOut() {
+    // do something
+  }
+
+}
