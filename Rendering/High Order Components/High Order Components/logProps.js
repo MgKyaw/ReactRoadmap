9d0650843f2e47e3963fbb7c64c8f22542a616da
MgKyaw <mgkyaw20112@gmail.com>
2026-03-29 23:52:29 +0700

    feat: Add logProps

--- Rendering/High Order Components/High Order Components/logProps.js
+++ Rendering/High Order Components/High Order Components/logProps.js
@@ -10,3 +10,18 @@ function logProps(InputComponent) {
 
 // EnhancedComponent will log whenever props are received
 const EnhancedComponent = logProps(InputComponent);
+
+// Add new logProps example
+
+function logProps(WrappedComponent) {
+  return class extends React.Component {
+    componentDidUpdate(prevProps) {
+      console.log('Current props: ', this.props);
+      console.log('Previous props: ', prevProps);
+    }
+    render() {
+      // Wraps the input component in a container, without mutating it. Good!
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+}
