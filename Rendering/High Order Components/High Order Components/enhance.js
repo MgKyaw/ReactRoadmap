da366fee976d918381e1dcb16aafd2add4c1f8c1
MgKyaw <mgkyaw20112@gmail.com>
2026-03-30 22:47:12 +0700

    feat: Add sample

--- Rendering/High Order Components/High Order Components/enhance.js
+++ Rendering/High Order Components/High Order Components/enhance.js
@@ -11,3 +11,13 @@ function enhance(WrappedComponent) {
   hoistNonReactStatic(Enhance, WrappedComponent);
   return Enhance;
 }
+
+// Instead of...
+MyComponent.someFunction = someFunction;
+export default MyComponent;
+
+// ...export the method separately...
+export { someFunction };
+
+// ...and in the consuming module, import both
+import MyComponent, { someFunction } from './MyComponent.js';
