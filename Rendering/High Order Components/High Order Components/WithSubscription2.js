143cc7b5b7e712ae8b9cbbfc09aa2c99e9913fc3
MgKyaw <mgkyaw20112@gmail.com>
2026-03-30 22:43:50 +0700

    feat: Add render

--- Rendering/High Order Components/High Order Components/WithSubscription2.js
+++ Rendering/High Order Components/High Order Components/WithSubscription2.js
@@ -7,3 +7,12 @@ function withSubscription(WrappedComponent) {
 function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
+
+// Add render
+render() {
+  // A new version of EnhancedComponent is created on every render
+  // EnhancedComponent1 !== EnhancedComponent2
+  const EnhancedComponent = enhance(MyComponent);
+  // That causes the entire subtree to unmount/remount each time!
+  return <EnhancedComponent />;
+}
