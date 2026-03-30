b02e81f95cca7064c242fbbd5f507438b8d27b12
MgKyaw <mgkyaw20112@gmail.com>
2026-03-30 22:44:45 +0700

    fix: Comment render function

--- Rendering/High Order Components/High Order Components/WithSubscription2.js
+++ Rendering/High Order Components/High Order Components/WithSubscription2.js
@@ -8,11 +8,11 @@ function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
 
-// Add render
-render() {
-  // A new version of EnhancedComponent is created on every render
-  // EnhancedComponent1 !== EnhancedComponent2
-  const EnhancedComponent = enhance(MyComponent);
-  // That causes the entire subtree to unmount/remount each time!
-  return <EnhancedComponent />;
-}
+// // Add render
+// render() {
+//   // A new version of EnhancedComponent is created on every render
+//   // EnhancedComponent1 !== EnhancedComponent2
+//   const EnhancedComponent = enhance(MyComponent);
+//   // That causes the entire subtree to unmount/remount each time!
+//   return <EnhancedComponent />;
+// }
