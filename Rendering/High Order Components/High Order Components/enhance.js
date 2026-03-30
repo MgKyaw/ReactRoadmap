2a877d27a5c56c9b27fb870c35d94ea11c58b729
MgKyaw <mgkyaw20112@gmail.com>
2026-03-30 22:46:35 +0700

    feat: Modify enhance

--- Rendering/High Order Components/High Order Components/enhance.js
+++ Rendering/High Order Components/High Order Components/enhance.js
@@ -1,6 +1,13 @@
+// function enhance(WrappedComponent) {
+//   class Enhance extends React.Component {/*...*/}
+//   // Must know exactly which method(s) to copy :(
+//   Enhance.staticMethod = WrappedComponent.staticMethod;
+//   return Enhance;
+// }
+
+import hoistNonReactStatic from 'hoist-non-react-statics';
 function enhance(WrappedComponent) {
   class Enhance extends React.Component {/*...*/}
-  // Must know exactly which method(s) to copy :(
-  Enhance.staticMethod = WrappedComponent.staticMethod;
+  hoistNonReactStatic(Enhance, WrappedComponent);
   return Enhance;
 }
