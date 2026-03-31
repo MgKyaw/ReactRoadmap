038671a6fbd5a4021c0d277977e14a22c0b331f6
MgKyaw <mgkyaw20112@gmail.com>
2026-03-31 23:24:17 +0700

    feat: Add enhance 3

---
+++ Rendering/High Order Components/High Order Components/enhance3.js
@@ -0,0 +1,9 @@
+// Instead of...
+MyComponent.someFunction = someFunction;
+export default MyComponent;
+
+// ...export the method separately...
+export { someFunction };
+
+// ...and in the consuming module, import both
+import MyComponent, { someFunction } from './MyComponent.js';
