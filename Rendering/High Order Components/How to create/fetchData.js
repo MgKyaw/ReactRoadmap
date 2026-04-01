f5698e711ee08c4acbde78e55acd76a5ace93345
MgKyaw <mgkyaw20112@gmail.com>
2026-04-01 20:22:12 +0700

    feat: Add fetchData

--- Rendering/High Order Components/How to create/fetchData.js
+++ Rendering/High Order Components/How to create/fetchData.js
@@ -1,11 +1,24 @@
+// const fetchData = () => {
+//   return { data: null };
+// };
+
+// const App = () => {
+//   const { data } = fetchData();
+
+//   if (!data) return <div>No data loaded yet.</div>;
+
+//   return <TodoList data={data} />;
+// };
+
 const fetchData = () => {
-  return { data: null };
+  return { data: [] };
 };
 
 const App = () => {
   const { data } = fetchData();
 
   if (!data) return <div>No data loaded yet.</div>;
+  if (!data.length) return <div>Data is empty.</div>;
 
   return <TodoList data={data} />;
 };
