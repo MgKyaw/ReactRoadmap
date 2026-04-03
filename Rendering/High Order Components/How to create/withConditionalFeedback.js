7b92ef28f327d2b824cd74275d14a163457152fd
MgKyaw <mgkyaw20112@gmail.com>
2026-04-03 22:48:11 +0700

    feat: Modify conditional feedback

--- Rendering/High Order Components/How to create/withConditionalFeedback.js
+++ Rendering/High Order Components/How to create/withConditionalFeedback.js
@@ -1,11 +1,40 @@
-const withConditionalFeedback = (Component) => (props) => {
-  if (props.isLoading) return <div>Loading data.</div>;
-  if (!props.data) return <div>No data loaded yet.</div>;
-  if (!props.data.length) return <div>Data is empty.</div>;
+// const withConditionalFeedback = (Component) => (props) => {
+//   if (props.isLoading) return <div>Loading data.</div>;
+//   if (!props.data) return <div>No data loaded yet.</div>;
+//   if (!props.data.length) return <div>Data is empty.</div>;
 
-  return <Component {...props} />;
-};
+//   return <Component {...props} />;
+// };
 
+// const App = () => {
+//   const { data, isLoading } = fetchData();
+
+//   return <TodoList data={data} isLoading={isLoading} />;
+// };
+
+// const BaseTodoList = ({ data }) => {
+//   return (
+//     <ul>
+//       {data.map((item) => (
+//         <TodoItem key={item.id} item={item} />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// const TodoList = withConditionalFeedback(BaseTodoList);
+
+
+const withConditionalFeedback = (dataEmptyFeedback) => (Component)
+  => (props) => {
+    if (props.isLoading) return <div>Loading data.</div>;
+    if (!props.data) return <div>No data loaded yet.</div>;
+
+    if (!props.data.length)
+      return <div>{dataEmptyFeedback || 'Data is empty.'}</div>;
+
+    return <Component {...props} />;
+  };
 const App = () => {
   const { data, isLoading } = fetchData();
 
@@ -22,4 +51,6 @@ const BaseTodoList = ({ data }) => {
   );
 };
 
-const TodoList = withConditionalFeedback(BaseTodoList);
+const TodoList = withConditionalFeedback('Todos are empty.')(
+  BaseTodoList
+);
