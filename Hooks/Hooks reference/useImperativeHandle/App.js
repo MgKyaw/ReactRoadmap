90506b4594d108eb2daeecfec3219ff2c9eaf7b1
Kyaw <mgkyaw20112@gmail.com>
2026-04-16 21:42:12 +0700

    Remove extra line

--- Hooks/Hooks reference/useImperativeHandle/App.js
+++ Hooks/Hooks reference/useImperativeHandle/App.js
@@ -8,7 +8,6 @@ export default function Form() {
     ref.current.focus();
     // This won't work because the DOM node isn't exposed:
     // ref.current.style.opacity = 0.5;
-    
   }
 
   return (
