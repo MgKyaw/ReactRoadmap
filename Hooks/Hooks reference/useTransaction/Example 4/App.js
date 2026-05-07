d446ea11e38c20bad7924f4db63f0d101d47158d
MgKyaw <mgkyaw20112@gmail.com>
2026-05-07 20:02:48 +0700

    feat: Add App.js

---
+++ Hooks/Hooks reference/useTransaction/Example 4/App.js
@@ -0,0 +1,35 @@
+import { Suspense, useState } from 'react';
+import TabButton from './TabButton.js';
+import AboutTab from './AboutTab.js';
+import PostsTab from './PostsTab.js';
+import ContactTab from './ContactTab.js';
+
+export default function TabContainer() {
+  const [tab, setTab] = useState('about');
+  return (
+    <Suspense fallback={<h1>🌀 Loading...</h1>}>
+      <TabButton
+        isActive={tab === 'about'}
+        action={() => setTab('about')}
+      >
+        About
+      </TabButton>
+      <TabButton
+        isActive={tab === 'posts'}
+        action={() => setTab('posts')}
+      >
+        Posts
+      </TabButton>
+      <TabButton
+        isActive={tab === 'contact'}
+        action={() => setTab('contact')}
+      >
+        Contact
+      </TabButton>
+      <hr />
+      {tab === 'about' && <AboutTab />}
+      {tab === 'posts' && <PostsTab />}
+      {tab === 'contact' && <ContactTab />}
+    </Suspense>
+  );
+}
