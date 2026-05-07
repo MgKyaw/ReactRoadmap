0d4ecb81dcd4975da1390343babd17ca5a957055
MgKyaw <mgkyaw20112@gmail.com>
2026-05-07 20:04:21 +0700

    feat: Add TabButton.js

---
+++ Hooks/Hooks reference/useTransaction/Example 5/TabButton.js
@@ -0,0 +1,20 @@
+import { useTransition } from 'react';
+
+export default function TabButton({ action, children, isActive }) {
+  const [isPending, startTransition] = useTransition();
+  if (isActive) {
+    return <b>{children}</b>
+  }
+  if (isPending) {
+    return <b className="pending">{children}</b>;
+  }
+  return (
+    <button onClick={() => {
+      startTransition(async () => {
+        await action();
+      });
+    }}>
+      {children}
+    </button>
+  );
+}
