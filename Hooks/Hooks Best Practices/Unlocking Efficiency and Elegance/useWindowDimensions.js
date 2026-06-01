dabd14c97b5a999dc0864f0ecce052fd93276a5e
MgKyaw <mgkyaw20112@gmail.com>
2026-06-01 12:48:27 +0700

    feat: Add notes inside useWindowDimensions

--- Hooks/Hooks Best Practices/Unlocking Efficiency and Elegance/useWindowDimensions.js
+++ Hooks/Hooks Best Practices/Unlocking Efficiency and Elegance/useWindowDimensions.js
@@ -1,5 +1,9 @@
 // Encapsulate Logic in Custom Hooks:
 
+// Custom Hooks provide a way to encapsulate and reuse logic across multiple components. By extracting shared functionality 
+// into a custom Hook, you promote code reusability and maintainability. 
+// Custom Hooks should start with the “use” prefix to indicate their Hook nature.
+
 function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState({
     width: window.innerWidth,
