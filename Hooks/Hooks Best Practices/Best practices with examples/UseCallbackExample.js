c0e2bf83dec524146d60be85b03e643fe5631356
MgKyaw <mgkyaw20112@gmail.com>
2026-06-06 11:45:53 +0700

    feat: Modify UseCallbackExample with inline function

--- Hooks/Hooks Best Practices/Best practices with examples/UseCallbackExample.js
+++ Hooks/Hooks Best Practices/Best practices with examples/UseCallbackExample.js
@@ -38,20 +38,59 @@
 //   <App />
 // )
 
-// With useCallback
+// // With useCallback
+
+// const App = () => {
+//   const [age, setAge] = useState(99)
+//   const handleClick = () => setAge(age + 1)
+//   const someValue = "someValue"
+//   const doSomething = useCallback(() => {
+//     return someValue
+//   }, [someValue])
+
+//   return (
+//     <div>
+//       <Age age={age} handleClick={handleClick} />
+//       <Instructions doSomething={doSomething} />
+//     </div>
+//   )
+// }
+
+// const Age = ({ age, handleClick }) => {
+//   return (
+//     <div>
+//       <div style={{ border: '2px', background: "papayawhip", padding: "1rem" }}>
+//         Today I am {age} Years of Age
+//       </div>
+//       <pre> - click the button below 👇 </pre>
+//       <button onClick={handleClick}>Get older! </button>
+//     </div>
+//   )
+// }
+
+// const Instructions = React.memo((props) => {
+//   return (
+//     <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
+//       <p>Follow the instructions above as closely as possible</p>
+//     </div>
+//   )
+// })
+
+// ReactDOM.render(<App />)
+
+// useCallback with inline function
 
 const App = () => {
   const [age, setAge] = useState(99)
   const handleClick = () => setAge(age + 1)
   const someValue = "someValue"
-  const doSomething = useCallback(() => {
-    return someValue
-  }, [someValue])
 
   return (
     <div>
       <Age age={age} handleClick={handleClick} />
-      <Instructions doSomething={doSomething} />
+      <Instructions doSomething={useCallback(() => {
+        return someValue
+      }, [someValue])} />
     </div>
   )
 }
@@ -68,7 +107,7 @@ const Age = ({ age, handleClick }) => {
   )
 }
 
-const Instructions = React.memo((props) => {
+const Instructions = memo((props) => {
   return (
     <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
       <p>Follow the instructions above as closely as possible</p>
@@ -76,4 +115,4 @@ const Instructions = React.memo((props) => {
   )
 })
 
-ReactDOM.render(<App />)
+render(<App />)
