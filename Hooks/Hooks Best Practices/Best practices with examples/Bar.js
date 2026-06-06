49b49802a627ed7b2adedc4d7a0053544397edcb
MgKyaw <mgkyaw20112@gmail.com>
2026-06-06 11:39:21 +0700

    feat: Add Bar.js

---
+++ Hooks/Hooks Best Practices/Best practices with examples/Bar.js
@@ -0,0 +1,29 @@
+const initializeState = () => ({
+  width: 100
+})
+
+// ✅ note how the value returned from the fn above overrides initialState below: 
+const initialState = { width: 15 }
+const reducer = (state, action) => {
+  switch (action) {
+    case 'plus':
+      return { width: state.width + 15 }
+    case 'minus':
+      return { width: Math.max(state.width - 15, 2) }
+    default:
+      throw new Error("what's going on?" )
+  }
+}
+
+const Bar = () => {
+  const [state, dispatch] = useReducer(reducer, initialState, initializeState)
+  return <>
+    <div style={{ background: 'teal', height: '30px', width: state.width }}></div>
+    <div style={{marginTop: '3rem'}}>
+        <button onClick={() => dispatch('plus')}>Increase bar size</button>
+        <button onClick={() => dispatch('minus')}>Decrease bar size</button>
+    </div>
+    </>
+}
+
+ReactDOM.render(Bar)
