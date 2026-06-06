10e2fba098e148c6aec0de2f4e01def9d08cd1f0
MgKyaw <mgkyaw20112@gmail.com>
2026-06-06 11:42:57 +0700

    feat: Update UseCallbackExample

--- Hooks/Hooks Best Practices/Best practices with examples/UseCallbackExample.js
+++ Hooks/Hooks Best Practices/Best practices with examples/UseCallbackExample.js
@@ -1,17 +1,59 @@
-const App = () => {
-    const [age, setAge] = useState(99)
-    const handleClick = () => setAge(age + 1)
-    const someValue = "someValue"
-    const doSomething = () => {
-      return someValue
-    }
+// const App = () => {
+//     const [age, setAge] = useState(99)
+//     const handleClick = () => setAge(age + 1)
+//     const someValue = "someValue"
+//     const doSomething = () => {
+//       return someValue
+//     }
 
-    return (
-      <div>
-        <Age age={age} handleClick={handleClick}/>
-        <Instructions doSomething={doSomething} />
-      </div>
-    )
+//     return (
+//       <div>
+//         <Age age={age} handleClick={handleClick}/>
+//         <Instructions doSomething={doSomething} />
+//       </div>
+//     )
+// }
+
+// const Age = ({ age, handleClick }) => {
+//   return (
+//     <div>
+//       <div style={{ border: '2px', background: "papayawhip", padding: "1rem" }}>
+//         Today I am {age} Years of Age
+//       </div>
+//       <pre> - click the button below 👇 </pre>
+//       <button onClick={handleClick}>Get older! </button>
+//     </div>
+//   )
+// }
+
+// const Instructions = React.memo((props) => {
+//   return (
+//     <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
+//       <p>Follow the instructions above as closely as possible</p>
+//     </div>
+//   )
+// })
+
+// ReactDOM.render (
+//   <App />
+// )
+
+// With useCallback
+
+const App = () => {
+  const [age, setAge] = useState(99)
+  const handleClick = () => setAge(age + 1)
+  const someValue = "someValue"
+  const doSomething = useCallback(() => {
+    return someValue
+  }, [someValue])
+
+  return (
+    <div>
+      <Age age={age} handleClick={handleClick} />
+      <Instructions doSomething={doSomething} />
+    </div>
+  )
 }
 
 const Age = ({ age, handleClick }) => {
@@ -34,6 +76,4 @@ const Instructions = React.memo((props) => {
   )
 })
 
-ReactDOM.render (
-  <App />
-)
+ReactDOM.render(<App />)
