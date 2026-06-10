0e828558f116b1a5c28d0a88e55470b51bccd869
MgKyaw <mgkyaw20112@gmail.com>
2026-06-10 23:14:41 +0700

    feat: Add App3.js

---
+++ Router/Routing/App3.js
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{ margin: 10 }}>
+        <NavLink
+          to='/'
+          style={({ isActive }) => ({
+            padding: 5,
+            ...(isActive ? { color: "red" } : {}),
+          })}
+        >
+            Home
+            </NavLink>
+        <NavLink
+          to='/about'
+          style={({ isActive }) => ({
+            padding: 5,
+            ...(isActive ? { color: "red" } : {}),
+          })}
+        >
+          About
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
