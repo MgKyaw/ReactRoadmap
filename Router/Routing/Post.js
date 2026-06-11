a006da59e3bc295a1eab2f9c8372c7aca769a8e1
MgKyaw <mgkyaw20112@gmail.com>
2026-06-11 19:48:54 +0700

    feat: Add Post.js

---
+++ Router/Routing/Post.js
@@ -0,0 +1,18 @@
+// interface Posts {
+//   [key: string]: {
+//     title: string;
+//     description: string;
+//   };
+// }
+
+// const BlogPosts: Posts = {
+//   "first-blog-post": {
+//     title: "First Blog Post",
+//     description: "Lorem ipsum dolor sit amet, consectetur adip.",
+//   },
+
+//   "second-blog-post": {
+//     title: "Second Blog Post",
+//     description: "Hello React Router v6",
+//   },
+// };
