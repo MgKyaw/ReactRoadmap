38ea6ab53516ede17a7894b22a9882cccb610b38
MgKyaw <mgkyaw20112@gmail.com>
2026-06-12 18:03:50 +0700

    feat: Modify Import at App2.js

--- Router/Routing/App2.js
+++ Router/Routing/App2.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Outlet, 
+  Link } from 'react-router';
 
 function Home() {
   return (
