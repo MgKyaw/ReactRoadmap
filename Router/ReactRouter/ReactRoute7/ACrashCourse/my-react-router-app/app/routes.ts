52cf6e888509f16c832c15d68a4388c81ac66a07
MgKyaw <mgkyaw20112@gmail.com>
2026-06-15 23:42:10 +0700

    feat: Add Advance Route Configuration

--- Router/ReactRouter/ReactRoute7/ACrashCourse/my-react-router-app/app/routes.ts
+++ Router/ReactRouter/ReactRoute7/ACrashCourse/my-react-router-app/app/routes.ts
@@ -2,9 +2,37 @@
 
 // export default [index("routes/home.tsx")] satisfies RouteConfig;
 
-import { type RouteConfig, route } from "@react-router/dev/routes";
+// Basic Route Configuration
+
+// import { type RouteConfig, route } from "@react-router/dev/routes";
+
+// export default [
+//   route("some/path", "./some/file.tsx"),
+//   // pattern ^           ^ module file
+// ] satisfies RouteConfig;
+
+// Advance Route Configuration
+
+import {
+  type RouteConfig,
+  route,
+  index,
+  layout,
+  prefix,
+} from "@react-router/dev/routes";
 
 export default [
-  route("some/path", "./some/file.tsx"),
-  // pattern ^           ^ module file
+  index("./home.tsx"),
+  route("about", "./about.tsx"),
+
+  layout("./auth/layout.tsx", [
+    route("login", "./auth/login.tsx"),
+    route("register", "./auth/register.tsx"),
+  ]),
+
+  ...prefix("concerts", [
+    index("./concerts/home.tsx"),
+    route(":city", "./concerts/city.tsx"),
+    route("trending", "./concerts/trending.tsx"),
+  ]),
 ] satisfies RouteConfig;
