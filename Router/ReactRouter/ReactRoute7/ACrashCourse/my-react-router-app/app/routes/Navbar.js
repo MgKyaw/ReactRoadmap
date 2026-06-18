b6932bcc3b7dfff95e05b545595c386355f1c685
MgKyaw <mgkyaw20112@gmail.com>
2026-06-18 20:00:17 +0700

    feat: Update Navbar.js

--- Router/ReactRouter/ReactRoute7/ACrashCourse/my-react-router-app/app/routes/Navbar.js
+++ Router/ReactRouter/ReactRoute7/ACrashCourse/my-react-router-app/app/routes/Navbar.js
@@ -3,18 +3,15 @@ import { NavLink } from "react-router";
 export function Navbar() {
   return (
     <nav>
-      <NavLink
-        to="/"
-        end
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        Home
+      <NavLink to="/home">
+        {({ isPending }) => (
+          <>Home {isPending && <span className="spinner">⏳</span>}</>
+        )}
       </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        About
+      <NavLink to="/about">
+        {({ isPending }) => (
+          <>About {isPending && <span className="spinner">⏳</span>}</>
+        )}
       </NavLink>
     </nav>
   );
