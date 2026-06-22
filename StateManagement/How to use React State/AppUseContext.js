69dc5c99271adae8cb855eb5ced196e6976c3b31
MgKyaw <mgkyaw20112@gmail.com>
2026-06-22 22:30:49 +0700

    feat: Add AppUseContext

---
+++ StateManagement/How to use React State/AppUseContext.js
@@ -0,0 +1,16 @@
+const App = () => {
+  const [filter, dispatchFilter] = useReducer(filterReducer, 'ALL');
+  const [todos, dispatchTodos] = useReducer(todoReducer, initialTodos);
+
+  const filteredTodos = todos.filter(todo => {
+    ...
+  });
+
+  return (
+    <TodoContext.Provider value={dispatchTodos}>
+      <Filter dispatch={dispatchFilter} />
+      <TodoList todos={filteredTodos} />
+      <AddTodo />
+    </TodoContext.Provider>
+  );
+};
