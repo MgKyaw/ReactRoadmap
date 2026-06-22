b95cd0776977dae38505c9c3d47675e71d260012
MgKyaw <mgkyaw20112@gmail.com>
2026-06-22 22:28:33 +0700

    feat: Add AppUseState

---
+++ StateManagement/How to use React State/AppUseState.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  const [text, setText] = React.useState('Hello React');
+
+  const handleChange = event => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <p>{text}</p>
+
+      <input type="text" value={text} onChange={handleChange} />
+    </>
+  );
+};
