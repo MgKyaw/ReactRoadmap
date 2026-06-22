f55d47503a0d70dfca7f8668c9e63291609a9f81
MgKyaw <mgkyaw20112@gmail.com>
2026-06-22 22:27:41 +0700

    feat: Add user.js

---
+++ StateManagement/How to use React State/user.js
@@ -0,0 +1,24 @@
+// 1)
+const isOpen = true;
+
+// 2)
+const user = {
+  id: '1',
+  firstName: 'Robin',
+  lastName: 'Wieruch',
+  email: 'hello@robinwieruch.com',
+};
+
+// 3)
+const users = {
+  2: {
+    firstName: 'Dennis',
+    lastName: 'Wieruch',
+    email: 'hello@denniswieruch.com',
+  },
+  3: {
+    firstName: 'Thomas',
+    lastName: 'Wieruch',
+    email: 'hello@thomaswieruch.com',
+  },
+};
