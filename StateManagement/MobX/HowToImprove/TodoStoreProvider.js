c65e5ba5dc67b1f6b2274810e24e12429e44422a
MgKyaw <mgkyaw20112@gmail.com>
2026-06-24 23:14:34 +0700

    feat: Add TodoStore

--- StateManagement/MobX/HowToImprove/TodoStoreProvider.js
+++ StateManagement/MobX/HowToImprove/TodoStoreProvider.js
@@ -1,24 +1,24 @@
-import { makeAutoObservable } from 'mobx';
-import React, { createContext, FC, useContext, useState } from 'react';
-type Item = {
-  id: string;
-  title: string;
-};
-export class TodoStore {
-  items: Item[] = [];
-  constructor() {
-    makeAutoObservable(this);
-  }
-  addItem = (title: string) => {
-    this.items = this.items.concat({ id: Date.now().toString(), title })
-  };
-  removeItem = (id: string) => {
-    this.items = this.items.filter(item => item.id !== id);
-  };
-}
-export const TodoContext = createContext<TodoStore>(null!);
-export const TodoProvider: FC<{}> = ({ children }) => {
-  const [store] = useState(new TodoStore());
-  return <TodoContext.Provider value={store}>{children}</TodoContext.Provider>;
-};
-export const useTodo = () => useContext(TodoContext);
+// import { makeAutoObservable } from 'mobx';
+// import React, { createContext, FC, useContext, useState } from 'react';
+// type Item = {
+//   id: string;
+//   title: string;
+// };
+// export class TodoStore {
+//   items: Item[] = [];
+//   constructor() {
+//     makeAutoObservable(this);
+//   }
+//   addItem = (title: string) => {
+//     this.items = this.items.concat({ id: Date.now().toString(), title })
+//   };
+//   removeItem = (id: string) => {
+//     this.items = this.items.filter(item => item.id !== id);
+//   };
+// }
+// export const TodoContext = createContext<TodoStore>(null!);
+// export const TodoProvider: FC<{}> = ({ children }) => {
+//   const [store] = useState(new TodoStore());
+//   return <TodoContext.Provider value={store}>{children}</TodoContext.Provider>;
+// };
+// export const useTodo = () => useContext(TodoContext);
