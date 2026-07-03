73a8a240e31c23b12e4dd7ecbbee7334f9211e04
MgKyaw <mgkyaw20112@gmail.com>
2026-07-03 19:19:47 +0700

    feat: Add className for Basket component

--- WritingCSS/Styling/Basket.js
+++ WritingCSS/Styling/Basket.js
@@ -15,15 +15,19 @@
 
 function Basket({ items, onClick }) {
   return (
-    <UnorderedList>
+    <ul>
       {items.map((item) => (
-        <ListItem key={item.id}>
+        <li key={item.id}>
           {item.name}
-          <Button type="button" onClick={() => onClick(item)}>
+          <button
+            type="button"
+            className="p-2 w-16 border border-solid border-gray-900 transition duration-100 ease-in hover:bg-gray-900 hover:text-white"
+            onClick={() => onClick(item)}
+          >
             {item.isFavorite ? 'Unlike' : 'Like'}
-          </Button>
-        </ListItem>
+          </button>
+        </li>
       ))}
-    </UnorderedList>
+    </ul>
   );
 }
