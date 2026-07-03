891d5a06f112d37d81aed343bae567797b7f14f3
MgKyaw <mgkyaw20112@gmail.com>
2026-07-03 19:20:53 +0700

    feat: Add new Basket

--- WritingCSS/Styling/Basket.js
+++ WritingCSS/Styling/Basket.js
@@ -13,6 +13,25 @@
 //   );
 // }
 
+// function Basket({ items, onClick }) {
+//   return (
+//     <ul>
+//       {items.map((item) => (
+//         <li key={item.id}>
+//           {item.name}
+//           <button
+//             type="button"
+//             className="p-2 w-16 border border-solid border-gray-900 transition duration-100 ease-in hover:bg-gray-900 hover:text-white"
+//             onClick={() => onClick(item)}
+//           >
+//             {item.isFavorite ? 'Unlike' : 'Like'}
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
 function Basket({ items, onClick }) {
   return (
     <ul>
@@ -21,8 +40,17 @@ function Basket({ items, onClick }) {
           {item.name}
           <button
             type="button"
-            className="p-2 w-16 border border-solid border-gray-900 transition duration-100 ease-in hover:bg-gray-900 hover:text-white"
             onClick={() => onClick(item)}
+            style={{
+              cursor: 'pointer',
+              border: '1px solid #1a202c',
+              padding: '8px',
+              minWidth: '64px',
+
+              background: 'transparent',
+
+              transition: 'all 0.1s ease-in',
+            }}
           >
             {item.isFavorite ? 'Unlike' : 'Like'}
           </button>
