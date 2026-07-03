e06a9ae62051a77e7b2ee7a073dc5eb47cad8dd1
MgKyaw <mgkyaw20112@gmail.com>
2026-07-03 19:21:28 +0700

    feat: Update Basket.js

--- WritingCSS/Styling/Basket.js
+++ WritingCSS/Styling/Basket.js
@@ -32,11 +32,52 @@
 //   );
 // }
 
+// function Basket({ items, onClick }) {
+//   return (
+//     <ul>
+//       {items.map((item) => (
+//         <li key={item.id}>
+//           {item.name}
+//           <button
+//             type="button"
+//             onClick={() => onClick(item)}
+//             style={{
+//               cursor: 'pointer',
+//               border: '1px solid #1a202c',
+//               padding: '8px',
+//               minWidth: '64px',
+
+//               background: 'transparent',
+
+//               transition: 'all 0.1s ease-in',
+//             }}
+//           >
+//             {item.isFavorite ? 'Unlike' : 'Like'}
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
 function Basket({ items, onClick }) {
   return (
-    <ul>
+    <ul
+      style={{
+        margin: '0',
+        padding: '0',
+        listStyleType: 'none',
+      }}
+    >
       {items.map((item) => (
-        <li key={item.id}>
+        <li
+          key={item.id}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '8px 0',
+          }}
+        >
           {item.name}
           <button
             type="button"
