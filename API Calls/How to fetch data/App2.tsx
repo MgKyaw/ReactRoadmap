443d955d928732d3f698414ab33eb6c8b524cf41
MgKyaw <mgkyaw20112@gmail.com>
2026-07-10 21:26:08 +0700

    feat: Add App2.tsx

--- API Calls/How to fetch data/App2.tsx
+++ API Calls/How to fetch data/App2.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
-const API = "https://hn.algolia.com/api/v1/search";
+// const API = "https://hn.algolia.com/api/v1/search";
 
-...
+// ...
 
-const App = () => {
-  const [data, setData] = useState<Story[]>([]);
+// const App = () => {
+//   const [data, setData] = useState<Story[]>([]);
 
-  useEffect(async () => {
-    const result = await axios(`${API}?query=react`);
+//   useEffect(async () => {
+//     const result = await axios(`${API}?query=react`);
 
-    setData(result.data.hits);
-  });
+//     setData(result.data.hits);
+//   });
 
-  return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
-};
+//   return (
+//     <ul>
+//       {data.map((item) => (
+//         <li key={item.objectID}>
+//           <a href={item.url}>{item.title}</a>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
 
-export default App;
+// export default App;
