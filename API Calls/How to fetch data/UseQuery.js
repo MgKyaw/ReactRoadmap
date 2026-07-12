1da342ddaba99e24019ce2910e2ebe3c95445183
MgKyaw <mgkyaw20112@gmail.com>
2026-07-12 21:10:25 +0700

    feat: Add UseQueryArgs

--- API Calls/How to fetch data/UseQuery.js
+++ API Calls/How to fetch data/UseQuery.js
@@ -24,3 +24,36 @@
 
 //   return { data, isLoading, isError };
 // };
+
+type UseQueryArgs<T> = {
+  queryKey: string[];
+  queryFn: () => Promise<T>;
+  initialData: T;
+};
+
+const useQuery = <T>({ queryFn, queryKey, initialData }: UseQueryArgs<T>) => {
+  const [data, setData] = useState<T>(initialData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await queryFn();
+
+        setData(result);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [...queryKey]);
+
+  return { data, isLoading, isError };
+};
