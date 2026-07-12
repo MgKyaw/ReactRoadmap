2ba2a73ed111fd8ca6816041e6c50970ccd85274
MgKyaw <mgkyaw20112@gmail.com>
2026-07-12 21:08:49 +0700

    feat: Modify UseQuery.js

--- API Calls/How to fetch data/UseQuery.js
+++ API Calls/How to fetch data/UseQuery.js
@@ -1,26 +1,26 @@
-const useQuery = (activeSearch: string) => {
-  const [data, setData] = useState<Story[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+// const useQuery = (activeSearch: string) => {
+//   const [data, setData] = useState<Story[]>([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isError, setIsError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setIsError(false);
+//       setIsLoading(true);
 
-      try {
-        const result = await axios(`${API}?query=${activeSearch}`);
+//       try {
+//         const result = await axios(`${API}?query=${activeSearch}`);
 
-        setData(result.data.hits);
-      } catch (error) {
-        setIsError(true);
-      }
+//         setData(result.data.hits);
+//       } catch (error) {
+//         setIsError(true);
+//       }
 
-      setIsLoading(false);
-    };
+//       setIsLoading(false);
+//     };
 
-    fetchData();
-  }, [activeSearch]);
+//     fetchData();
+//   }, [activeSearch]);
 
-  return { data, isLoading, isError };
-};
+//   return { data, isLoading, isError };
+// };
