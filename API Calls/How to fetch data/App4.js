501ac8d5cfabdc0cb832a777b7b6a5f166296496
MgKyaw <mgkyaw20112@gmail.com>
2026-07-12 21:06:32 +0700

    feat: Add App4.js

--- API Calls/How to fetch data/App4.js
+++ API Calls/How to fetch data/App4.js
@@ -1,35 +1,35 @@
-const App = () => {
-  const [data, setData] = useState<Story[]>([]);
-  const [search, setSearch] = useState("");
-  const [activeSearch, setActiveSearch] = useState("react");
+// const App = () => {
+//   const [data, setData] = useState<Story[]>([]);
+//   const [search, setSearch] = useState("");
+//   const [activeSearch, setActiveSearch] = useState("react");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(`${API}?query=${activeSearch}`);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const result = await axios(`${API}?query=${activeSearch}`);
 
-      setData(result.data.hits);
-    };
+//       setData(result.data.hits);
+//     };
 
-    fetchData();
-  }, [activeSearch]);
+//     fetchData();
+//   }, [activeSearch]);
 
-//   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setSearch(event.target.value);
+// //   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+// //     setSearch(event.target.value);
+// //   };
+
+//   const handleSearchSubmit = () => {
+//     setActiveSearch(search);
+//     setSearch("");
 //   };
 
-  const handleSearchSubmit = () => {
-    setActiveSearch(search);
-    setSearch("");
-  };
+//   return (
+//     <>
+//       <input type="text" value={search} onChange={handleSearchChange} />
+//       <button type="button" onClick={handleSearchSubmit}>
+//         Search
+//       </button>
 
-  return (
-    <>
-      <input type="text" value={search} onChange={handleSearchChange} />
-      <button type="button" onClick={handleSearchSubmit}>
-        Search
-      </button>
-
-      <ul>...</ul>
-    </>
-  );
-};
+//       <ul>...</ul>
+//     </>
+//   );
+// };
