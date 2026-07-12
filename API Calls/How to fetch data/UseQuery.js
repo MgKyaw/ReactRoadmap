0d370a55e662dc7e92f9ae2e476fd78e0ffc7415
MgKyaw <mgkyaw20112@gmail.com>
2026-07-12 21:05:14 +0700

    feat: Add UseQuery.js

---
+++ API Calls/How to fetch data/UseQuery.js
@@ -0,0 +1,26 @@
+const useQuery = (activeSearch: string) => {
+  const [data, setData] = useState<Story[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(`${API}?query=${activeSearch}`);
+
+        setData(result.data.hits);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [activeSearch]);
+
+  return { data, isLoading, isError };
+};
