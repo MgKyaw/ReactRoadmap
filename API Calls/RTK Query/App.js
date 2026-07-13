2dc0824882ac3fb34b95983ff5454d6ed2da0be6
MgKyaw <mgkyaw20112@gmail.com>
2026-07-13 22:49:08 +0700

    feat: Add App.js

---
+++ API Calls/RTK Query/App.js
@@ -0,0 +1,5 @@
+import { createApi } from '@reduxjs/toolkit/query'
+
+/* React-specific entry point that automatically generates
+   hooks corresponding to the defined endpoints */
+import { createApi } from '@reduxjs/toolkit/query/react'
